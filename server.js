9ad38654ef7694481ea41cea9f3de98d65392905
sanitizepeople <daffarozano@gmail.com>
2023-06-30 00:05:43 +0700

    a

--- server.js
+++ server.js
@@ -3,32 +3,10 @@ import "babel-polyfill";
 import cors from "cors";
 import env from "./env.js";
 import adminRoute from "./public/routes/adminRoute.js";
-import path from "path";
-import { fileURLToPath } from "url";
-import bodyParser from "body-parser";
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-
-const __dirname = path.dirname(__filename);
 
 app.use(cors());
-let intialPath = path.join(__dirname, "public");
-
-app.use(bodyParser.json());
-app.use(express.static(intialPath));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(intialPath, "index.html"));
-});
-
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(intialPath, "login-as.html"));
-});
-
-app.get("/signup", (req, res) => {
-  res.sendFile(path.join(intialPath, "sign-up.html"));
-});
 
 app.use(
   express.urlencoded({
